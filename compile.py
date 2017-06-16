151a1f2375a2823ec6630da538c7a39d16d08729
MacLean Kuraitis <maclean.kuraitis@gmail.com>
2017-06-16 02:29:22 -0400

    Made a barebones compiler script that works.

--- compile.py
+++ compile.py
@@ -1,7 +1,32 @@
 # Let's Build a Wonderswan Compiler!
+import os
+import sys
 
-# Delete old ROM
+# Take in path arguments
+if len(sys.argv) != 3:
+	input = "buildme"
+	output = "test"
+else:
+	input = str(sys.argv[1])
+	output = str(sys.argv[2])
 
-# Make graphics assets
+# TBD!!! Automatically populate .bmp files based on /gfx directory
+
+bmp2ws_dir = "/Users/maclean/Documents/Programming/Wonderswan/swandriving/wonderswandev/bin/bmp2swan.exe"
+#bmp2ws_c = "wine " + bmp2ws_dir 
+
+compile_c = "nasm -f bin -o " + output + ".wsc " + input + ".asm"
+
+runemu_dir = "/Users/maclean/Documents/Programming/Wonderswan/cygne21a-win/cygne.exe"
+runemu_c = "wine " + runemu_dir
+
+# TBD!!! Delete old ROM
+
+# TBD!!! Make graphics assets (likely needs a loop, look into other exe)
+#os.system(bmp2ws_c)
 
 # Compile ROM
+os.system(compile_c)
+
+# Open Emulator
+os.system(runemu_c)
